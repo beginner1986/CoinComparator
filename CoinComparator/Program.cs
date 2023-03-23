46d95f2c0e0a7bf8d4db87b9768aec20f10bdf7a
beginner1986 <beginner1986@gmail.com>
2023-03-23 22:06:25 +0100

    Canny edge detection added. Parameters adjusted

--- CoinComparator/Program.cs
+++ CoinComparator/Program.cs
@@ -12,17 +12,21 @@ internal class Program
         if (img is null)
             return;
 
-        Mat img_gray = new();
-        CvInvoke.CvtColor(img, img_gray, ColorConversion.Bgr2Gray);
+        Mat imgGray = new();
+        CvInvoke.CvtColor(img, imgGray, ColorConversion.Bgr2Gray);
 
-        Mat img_blur = new();
-        CvInvoke.GaussianBlur(img_gray, img_blur, new System.Drawing.Size(7, 7), 0);
+        Mat imgBlur = new();
+        CvInvoke.GaussianBlur(imgGray, imgBlur, new System.Drawing.Size(7, 7), 0);
 
-        Mat img_sobel= new();
-        CvInvoke.Sobel(img_blur, img_sobel, DepthType.Cv64F, 1, 1, 3);
+        Mat imgSobel= new();
+        CvInvoke.Sobel(imgBlur, imgSobel, DepthType.Cv64F, 0, 1, 3);
+        
+        Mat imgCanny = new();
+        CvInvoke.Canny(imgBlur, imgCanny, 50, 100);
 
         CvInvoke.Imshow("Input image", img);
-        CvInvoke.Imshow("Output image", img_sobel);
+        CvInvoke.Imshow("Output image", imgSobel);
+        CvInvoke.Imshow("Output image2", imgCanny);
         CvInvoke.WaitKey(0);
         CvInvoke.DestroyAllWindows();
     }
