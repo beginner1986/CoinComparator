4a24d0920a8c0b08d6a570add2918d9ffd864b26
beginner1986 <beginner1986@gmail.com>
2023-03-23 21:41:40 +0100

    Gray image gaussian blur added

--- CoinComparator/Program.cs
+++ CoinComparator/Program.cs
@@ -8,14 +8,18 @@ internal class Program
     private static void Main(string[] args)
     {
         Mat img = CvInvoke.Imread("test_img.png");
-        Mat img_gray = new Mat();
-        CvInvoke.CvtColor(img, img_gray, ColorConversion.Bgr2Gray);
 
         if (img is null)
             return;
 
+        Mat img_gray = new();
+        CvInvoke.CvtColor(img, img_gray, ColorConversion.Bgr2Gray);
+
+        Mat img_blur = new();
+        CvInvoke.GaussianBlur(img_gray, img_blur, new System.Drawing.Size(3, 3), 0);
+
         CvInvoke.Imshow("Input image", img);
-        CvInvoke.Imshow("Gray image", img_gray);
+        CvInvoke.Imshow("Output image", img_blur);
         CvInvoke.WaitKey(0);
         CvInvoke.DestroyAllWindows();
     }
