6300f549a752af34e5e0649a032967d27ad7d7d4
beginner1986 <beginner1986@gmail.com>
2023-03-23 21:35:43 +0100

    Input image converted to grayscale

--- CoinComparator/Program.cs
+++ CoinComparator/Program.cs
@@ -7,16 +7,16 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string window = "Coin Comparator";
-        CvInvoke.NamedWindow(window);
-
         Mat img = CvInvoke.Imread("test_img.png");
+        Mat img_gray = new Mat();
+        CvInvoke.CvtColor(img, img_gray, ColorConversion.Bgr2Gray);
 
         if (img is null)
             return;
 
-        CvInvoke.Imshow(window, img);
+        CvInvoke.Imshow("Input image", img);
+        CvInvoke.Imshow("Gray image", img_gray);
         CvInvoke.WaitKey(0);
-        CvInvoke.DestroyWindow(window);
+        CvInvoke.DestroyAllWindows();
     }
 }
