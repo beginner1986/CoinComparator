6ddd9fd6eec74a6db50f2134ae108e3c2edf1007
beginner1986 <beginner1986@gmail.com>
2023-03-23 21:49:59 +0100

    Sobel edge detection used. Blur parameters adjusted.

--- CoinComparator/Program.cs
+++ CoinComparator/Program.cs
@@ -16,10 +16,13 @@ internal class Program
         CvInvoke.CvtColor(img, img_gray, ColorConversion.Bgr2Gray);
 
         Mat img_blur = new();
-        CvInvoke.GaussianBlur(img_gray, img_blur, new System.Drawing.Size(3, 3), 0);
+        CvInvoke.GaussianBlur(img_gray, img_blur, new System.Drawing.Size(7, 7), 0);
+
+        Mat img_sobel= new();
+        CvInvoke.Sobel(img_blur, img_sobel, DepthType.Cv64F, 1, 1, 3);
 
         CvInvoke.Imshow("Input image", img);
-        CvInvoke.Imshow("Output image", img_blur);
+        CvInvoke.Imshow("Output image", img_sobel);
         CvInvoke.WaitKey(0);
         CvInvoke.DestroyAllWindows();
     }
