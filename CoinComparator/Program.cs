b178d9fb742cb7733553adef40f306c5f8d68eed
beginner1986 <beginner1986@gmail.com>
2023-03-23 21:30:48 +0100

    Image file reading implemented

--- CoinComparator/Program.cs
+++ CoinComparator/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string window = "Coin Comparator";
+        CvInvoke.NamedWindow(window);
+
+        Mat img = CvInvoke.Imread("test_img.png");
+
+        if (img is null)
+            return;
+
+        CvInvoke.Imshow(window, img);
+        CvInvoke.WaitKey(0);
+        CvInvoke.DestroyWindow(window);
+    }
+}
